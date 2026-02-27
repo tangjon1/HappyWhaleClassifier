18e78248c3855118a6bc3b62644022770e77a72a
tangjon1 <81594929+tangjon1@users.noreply.github.com>
2026-02-26 19:41:13 -0800

    Transferred relevant code sections to new script

---
+++ ml_whale.py
@@ -0,0 +1,441 @@
+#!/usr/bin/env python
+
+import argparse
+
+## Importing packages - Please DO NOT alter this box ##
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
+from torch.utils.data.sampler import WeightedRandomSampler
+torch.manual_seed(0)
+
+from captum.attr import IntegratedGradients
+from captum.attr import DeepLift
+from captum.attr import NoiseTunnel
+from captum.attr import visualization as viz
+
+import torchvision
+from torchvision import datasets, transforms
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+import os
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from scipy.ndimage import binary_erosion
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+import wandb #comment this out if you are not using weights and biases
+import random #comment this out if you are not using weights and biases
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Simple example of argparse usage.")
+parser.add_argument("-e", "--epochs", help="Num epochs", type=int)
+parser.add_argument("-l", "--learn-rate", help="Learning rate", type=float)
+parser.add_argument("-b", "--batch", help="Batch size", type=int)
+parser.add_argument("-o", "--output-dir", help="Directory for output files and images")
+
+args = parser.parse_args()
+
+
+# Set device to cuda if it's available otherwise default to "cpu"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+images = "/projects/bgmp/shared/Bi625/ML_Assignment/Datasets/Whale_species/species"
+
+# We must turn our images into tensors, resize, and normalize them 
+# We can also add additional transformations to images 
+transform = transforms.Compose([transforms.ToTensor(), 
+                                #We will start with a model called Resnet18 that is optimized for 224x224 images
+                                #It is set to a very SMALL size initially so the model will train fast in class
+                                transforms.Resize([32,32]),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Normalize the data, these are the values that ResNet suggests based on their training data (natural scences)
+                               ])
+all_images = datasets.ImageFolder(images, transform)
+print(all_images)
+
+all_images = datasets.ImageFolder(images, transform )
+print(len(all_images))
+print(all_images)
+
+print(all_images.class_to_idx)
+
+def show_random_dataset_image(dataset):
+    idx = np.random.randint(0, len(dataset))    # take a random sample
+    img, mask = dataset[idx]                    # get the image and the nuclei masks
+    f, axarr = plt.subplots(1, 2)               # make two plots on one figure
+    axarr[0].imshow(img[0], cmap="viridis")                     # show the image, cmap is the color map that the image is being shown in
+    #axarr[1].imshow(mask[0])                    # show the masks
+    _ = [ax.axis('off') for ax in axarr]        # remove the axes
+    print('Image size is %s' % {img[0].shape})
+    print(img.shape)
+    plt.show()
+
+show_random_dataset_image(all_images)
+
+## ADD YOUR TRANSFORMATION HERE
+transform = transforms.Compose([
+            transforms.Resize([224,224]), # Resize the image as our model is optimized for 224x224 pixels
+            transforms.ColorJitter(brightness=.5, hue=.3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]) # Normalize the data, these are the values that ResNet suggests based on their training data (natural scences))
+
+all_images = datasets.ImageFolder(images, transform )
+show_random_dataset_image(all_images)
+
+train_size = int(0.7 * len(all_images))
+val_size = int(0.15 * len(all_images))
+test_size = len(all_images) - (train_size + val_size)
+print(train_size, val_size, test_size)
+assert train_size + val_size + test_size == len(all_images)
+
+train_set, val_set, test_set = torch.utils.data.random_split(all_images, [train_size, val_size, test_size])
+
+def _get_weights(subset,full_dataset):
+    ys = np.array([y for _, y in subset])
+    counts = np.bincount(ys)
+    label_weights = 1.0 / counts
+    weights = label_weights[ys]
+
+    print("Number of images per class:")
+    for c, n, w in zip(full_dataset.classes, counts, label_weights):
+        print(f"\t{c}:\tn={n}\tweight={w}")
+        
+    return weights
+
+
+train_weights = _get_weights(train_set,all_images)
+train_sampler = WeightedRandomSampler(train_weights, len(train_weights))
+
+train_loader = DataLoader(train_set, batch_size=48, drop_last=True, sampler=train_sampler)
+val_loader = DataLoader(val_set, batch_size=48, drop_last=True, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=48, drop_last=True, shuffle=True)
+
+learning_rate=1e-3
+batchsize=48
+epochs=5
+
+## First, we create the basic block that will be used in our residual net
+class BasicBlock(nn.Module):
+    # Initializing method for the basic block (It's OOP!)
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BasicBlock, self).__init__()
+        ## Conv1: convolution layer, batch normalization, ReLU activation
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        ## Conv2: convolution layer, batch normalization
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        ## Shortcut connection: adds input to output
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    # The forward method calls on each layer
+    def forward(self, x):
+        ## Conv1: convolution layer, batch normalization, ReLU activation
+        out = self.conv1(x) 
+        out = self.bn1(out)
+        out = self.relu(out)
+        ## Conv2: convolution layer, batch normalization
+        out = self.conv2(out)
+        out = self.bn2(out)
+        ## Shortcut connection
+        out += self.shortcut(x)
+        ## Final activation
+        out = self.relu(out)
+        return out
+
+
+## Next, we put together these building blocks and create our residual net
+class ResNet18(nn.Module):
+    # specify the number of classes that we are predicting
+    def __init__(self, num_classes=6):
+        super(ResNet18, self).__init__()
+        # In channels = Num pixels in H + Num pixels in W
+        self.in_channels = 64
+        
+        # First convolution set (convolution, batch norm, relu)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Our building blocks
+        # The numbers correspond to the matrix shape
+        ### We increase the number of filters/channels (i.e., the first number) as we go
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+
+        # Average pooling 
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # Final layer that makes the classification
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # First convolution set (convolution, batch norm, relu)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        # Our 4 building blocks
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        # Final layer that makes the classification
+        out = self.fc(out)
+        return out
+
+## Getting our model and transferring it to the GPU
+model = ResNet18().to(device)
+print(model)
+
+#define loss function & optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+#wandb.init(
+#    project="BGMP_HappyWhale",
+#    name="Hope-HappyWhale-SpeciesDataset-hyperparameters", ### Update with your name!
+#    config={"learning rate":0.001,
+#        "architecture": "CNN",
+#        "dataset": "Species",
+#        "epochs":5}
+#)
+
+num_epochs = epochs
+train_losses, train_acc_list, val_losses, val_acc_list = [], [], [],[]
+
+for epoch in range(num_epochs):
+    # Setting model to "training mode"
+    model.train()
+    running_loss = 0.0
+    correct, total = 0, 0
+    batch=0
+
+    # For each batch of data within the loader
+    for inputs, labels in train_loader:
+        # Send our input images and their labels to the GPU
+        inputs, labels = inputs.to(device), labels.to(device)
+        # Zero the gradients
+        optimizer.zero_grad()
+        # Inputting our training images into the model
+        # and Predicting the image classification label
+        outputs = model(inputs)
+        # Figuring out the loss from our predictions
+        loss = criterion(outputs, labels)
+        # Compute gradients (aka backward pass)
+        loss.backward()
+        # Update model parameters
+        optimizer.step()
+
+        # Adding the loss to our running sum
+        # Loss is calculated for each batch within an epoch
+        running_loss += loss.item() * inputs.size(0)
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+        batch+=1
+        print(f'Epoch [{epoch+1}/{num_epochs}] | Batch #{batch} | Batch Accuracy {(correct/total)*100:.2f}%')
+
+
+    # Getting metrics for our training pass 
+    train_loss = running_loss / len(train_loader.dataset)
+    train_acc = 100. * correct / total
+    train_losses.append(train_loss)
+    train_acc_list.append(train_acc)
+    
+    # Switching our model to "evaluation mode"
+    model.eval()
+    running_loss = 0.0
+    correct, total = 0, 0
+    # Disable gradient calculation b/c we are evalulating the model
+    with torch.no_grad():
+        # Load in batches of our validation data
+        for inputs, labels in val_loader:
+            # Send test images and labels to the GPU
+            inputs, labels = inputs.to(device), labels.to(device)
+            # Predict the image classification label
+            outputs = model(inputs)
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            # Figuring out how many predicted labels = true labels
+            correct += predicted.eq(labels).sum().item()
+
+            # Figuring out the loss from our predictions
+            loss = criterion(outputs, labels)
+            # Adding the loss to our running sum
+            # Loss is calculated for each batch within an epoch
+            running_loss += loss.item() * inputs.size(0)
+    # Getting our accuracy from our test data
+    val_acc = 100. * correct / total
+    val_acc_list.append(val_acc)
+    # Getting the loss from our test data
+    val_loss = running_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+    
+    print(f'Epoch [{epoch+1}/{num_epochs}] Train Loss: {train_loss:.4f} | Train Acc: {train_acc:.2f}% | Val Acc: {val_acc:.2f}% | Val Loss: {val_loss:.2f}%')
+
+    # log metrics to wandb
+    #wandb.log({"validation_accuracy": val_acc, "validation_loss": val_loss, "train_loss":train_loss})
+
+#wandb.finish()
+
+plt.plot(range(num_epochs), val_acc_list, color = "magenta")
+plt.xlabel("Epoch number")
+plt.ylabel("Valiation accuracy")
+plt.show()  
+
+plt.plot(range(num_epochs), val_losses, color = "purple")
+plt.xlabel("Epoch number")
+plt.ylabel("Valiation loss")
+plt.show() 
+
+# predict the test dataset
+def predict(model, dataset):
+    dataset_prediction = []
+    dataset_groundtruth = []
+    model = model
+    with torch.no_grad():
+        for x, y_true in dataset:
+            inp = x[None]
+            y_pred = model(inp)
+            dataset_prediction.append(y_pred.argmax().cpu().numpy())
+            dataset_groundtruth.append(y_true)
+    
+    return np.array(dataset_prediction), np.array(dataset_groundtruth)
+            
+    # create seaborn heatmap with required labels
+    ax=sns.heatmap(cm, annot=annot, fmt='', vmax=30, xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+    ax.set_title(title)
+
+# Plot confusion matrix 
+# orginally from Runqi Yang; 
+# see https://gist.github.com/hitvoice/36cf44689065ca9b927431546381a3f7
+def cm_analysis(y_true, y_pred, title, figsize=(10,10)):
+    """
+    Generate matrix plot of confusion matrix with pretty annotations.
+    The plot image is saved to disk.
+    args: 
+      y_true:    true label of the data, with shape (nsamples,)
+      y_pred:    prediction of the data, with shape (nsamples,)
+      filename:  filename of figure file to save
+      labels:    string array, name the order of class labels in the confusion matrix.
+                 use `clf.classes_` if using scikit-learn models.
+                 with shape (nclass,).
+      ymap:      dict: any -> string, length == nclass.
+                 if not None, map the labels & ys to more understandable strings.
+                 Caution: original y_true, y_pred and labels must align.
+      figsize:   the size of the figure plotted.
+    """
+    labels = ['Beluga','Common dolphin', 'False killer whale', 'Fin whale', 'Gray whale','Humpback whale']
+    cm = confusion_matrix(y_true, y_pred)
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=labels, columns=labels)
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+                              
+    x_axis_labels = ['Beluga','Common dolphin', 'False killer whale', 'Fin whale', 'Gray whale','Humpback whale'] # labels for x-axis
+    y_axis_labels = ['Beluga','Common dolphin', 'False killer whale', 'Fin whale', 'Gray whale','Humpback whale'] # labels for y-axis
+    ax=sns.heatmap(cm, annot=annot, fmt='', vmax=30, xticklabels=x_axis_labels, yticklabels=y_axis_labels, cmap = "viridis")
+    ax.set_title(title)
+
+## This plot only contains the test set of data
+## The test set of data has not been seen by the model yet
+y_pred, y_true = predict(model, test_set)
+
+print(y_true)
+cm_analysis(y_true, y_pred, "Confusion matrix")
+
+def visualize_ig(idx, 
+                 _train_dataset=train_set, 
+                 _test_dataset=test_set):
+        
+    # Get corresponding input and target tensors:
+    input_tensor = _test_dataset[idx][0].unsqueeze(0)
+    input_tensor.requires_grad = True
+    target = _test_dataset[idx][1]
+    
+    # We will use the IntegratedGradients algorithm:
+    algorithm = IntegratedGradients(model)
+    
+    # First we clear the gradients from the model:
+    model.zero_grad()
+    
+    # Run attribution:
+    attr_ig, delta = algorithm.attribute(input_tensor,
+                                          target=target,
+                                          baselines=input_tensor * 0,
+                                          return_convergence_delta=True
+                                        )
+    
+    # Integrated Gradients:
+    attr_ig = np.transpose(attr_ig[0].cpu().detach().numpy(), (1, 2, 0))
+    
+    # Original image:
+    original_image = np.transpose((_test_dataset[idx][0].detach().numpy() * 0.5) + 0.5, (1, 2, 0))
+    
+    print("Annotated whale species", test_true[idx])
+    plt.imshow(original_image)
+    
+    
+    print("Predicted whale species", test_pred[idx])
+
+
+    # This visualises the attribution of labels to 
+    viz.visualize_image_attr(attr_ig, 
+                             original_image, 
+                             method="blended_heat_map",
+                             sign="all",
+                             show_colorbar=True, 
+                             title="Overlayed Integrated Gradients")
+
+test_pred, test_true = predict(model, test_set)
+
+visualize_ig(0)
+visualize_ig(8)
+visualize_ig(20)
+visualize_ig(32)
